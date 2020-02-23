bc6495bbda0df2c8d2394271239395c08e8ed9c7
Tim 'mithro' Ansell <me@mith.ro>
2020-02-23 11:33:47 -0800

    Updating data to v0.0-1746-gdbb13f31
    Updated to 0.0.post1746 based on dbb13f31370a743633dc94d3639d55c8c4d74e1d from https://github.com/enjoy-digital/black-parrot.git.
    > commit dbb13f31370a743633dc94d3639d55c8c4d74e1d
    > Author: Florent Kermarrec <florent@enjoy-digital.fr>
    > Date:   Sun Feb 9 19:44:22 2020 +0100
    >
    >     remove submodules
    >
    
    Updated using v0.0-13-gb1be429 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/blackparrot/__init__.py
+++ litex/data/cpu/blackparrot/__init__.py
@@ -4,10 +4,10 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/enjoy-digital/black-parrot.git"
 git_hash = "dbb13f31370a743633dc94d3639d55c8c4d74e1d"
 git_describe = "v0.0-1746-gdbb13f31"
-version_str = "0.0.1746"
-version_tuple = (0, 0, 1746)
+version_str = "0.0.post1746"
+version_tuple = (0, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.1746")
+    pversion = V("0.0.post1746")
 except ImportError:
     pass
