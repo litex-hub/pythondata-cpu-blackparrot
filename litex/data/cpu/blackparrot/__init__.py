a7c2d9b664d0ea54d3fe486b57248172d65f2a6a
LiteX <robot@timvideos.us>
2020-04-06 15:27:39 +0000

    Updating module to 0.0.post1789
    Updated data to v0.0-1746-gdbb13f31 based on dbb13f31370a743633dc94d3639d55c8c4d74e1d from https://github.com/enjoy-digital/black-parrot.git.
    > commit dbb13f31370a743633dc94d3639d55c8c4d74e1d
    > Author: Florent Kermarrec <florent@enjoy-digital.fr>
    > Date:   Sun Feb 9 19:44:22 2020 +0100
    >
    >     remove submodules
    >
    
    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/blackparrot/__init__.py
+++ litex/data/cpu/blackparrot/__init__.py
@@ -2,12 +2,40 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/enjoy-digital/black-parrot.git"
-git_hash = "dbb13f31370a743633dc94d3639d55c8c4d74e1d"
-git_describe = "v0.0-1746-gdbb13f31"
-version_str = "0.0.post1746"
-version_tuple = (0, 0)
+
+# Module version
+version_str = "0.0.post1789"
+version_tuple = (0, 0, 1789)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1746")
+    pversion = V("0.0.post1789")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post1746"
+data_version_tuple = (0, 0, 1746)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post1746")
+except ImportError:
+    pass
+data_git_hash = "dbb13f31370a743633dc94d3639d55c8c4d74e1d"
+data_git_describe = "v0.0-1746-gdbb13f31"
+data_git_msg = """\
+commit dbb13f31370a743633dc94d3639d55c8c4d74e1d
+Author: Florent Kermarrec <florent@enjoy-digital.fr>
+Date:   Sun Feb 9 19:44:22 2020 +0100
+
+    remove submodules
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post43"
+tool_version_tuple = (0, 0, 43)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post43")
 except ImportError:
     pass
--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import blackparrot
 
-print("Found blackparrot @ version", blackparrot.version_str, "("+blackparrot.git_hash+")")
+print("Found blackparrot @ version", blackparrot.version_str, "(with data", blackparrot.data_version_str, ")")
+print()
 print("Data is in", blackparrot.data_location)
 assert os.path.exists(blackparrot.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(blackparrot.data_location)))
+print("Data is version", blackparrot.data_version_str, blackparrot.data_git_hash)
+print("-"*75)
+print(blackparrot.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(blackparrot.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), blackparrot.data_location)
+        print(" -", path)
