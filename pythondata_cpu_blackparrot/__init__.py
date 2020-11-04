4264d9b0ee43dbb04a94260a6cf9063202996537
LiteX <robot@timvideos.us>
2020-11-04 22:26:37 +0000

    Updating pythondata-cpu-blackparrot to 0.0.post1817
    Updated data to v0.0-1751-g03d25145 based on 03d2514542557199a79373443c8e405f0bfee53f from https://github.com/enjoy-digital/black-parrot.git.
    > commit 03d2514542557199a79373443c8e405f0bfee53f
    > Merge: 30b599ca 10401383
    > Author: enjoy-digital <florent@enjoy-digital.fr>
    > Date:   Fri May 15 12:47:57 2020 +0200
    >
    >     Merge pull request #2 from scanakci/master
    >
    >     Missing files due to gitignore
    >
    
    Updated using 0.0.post66 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_blackparrot/__init__.py
+++ pythondata_cpu_blackparrot/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "system_verilog")
 src = "https://github.com/enjoy-digital/black-parrot.git"
 
 # Module version
-version_str = "0.0.post1811"
-version_tuple = (0, 0, 1811)
+version_str = "0.0.post1817"
+version_tuple = (0, 0, 1817)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post1811")
+    pversion = V("0.0.post1817")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Fri May 15 12:47:57 2020 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post60"
-tool_version_tuple = (0, 0, 60)
+tool_version_str = "0.0.post66"
+tool_version_tuple = (0, 0, 66)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post60")
+    ptool_version = V("0.0.post66")
 except ImportError:
     pass
 
