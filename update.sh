5573b2f80e4d3b8cda0c8788bc98b2ac173b319d
developandplay <34752929+developandplay@users.noreply.github.com>
2021-07-18 13:10:01 +0200

    Add update script

--- update.sh
+++ update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+cd pythondata_cpu_blackparrot/system_verilog/
+rm -rf black-parrot
+git clone https://github.com/black-parrot/black-parrot.git
+cd black-parrot
+git checkout top_dev
+git submodule update --init external/basejump_stl
+git submodule update --init external/HardFloat
+
 ## Set common variables
-BP=$(dirname $(dirname ${BASH_SOURCE[0]}))/black-parrot
+BP=$PWD
 BP_LITEX_DIR=$BP/bp_litex
 
 BP_COMMON_DIR=$BP/bp_common
@@ -17,3 +25,9 @@ LITEX_SIMU_DIR=$BP_LITEX_DIR/simulation
 sed -i "s/localparam dram_base_addr_gp         = 40'h00_8000_0000;/localparam dram_base_addr_gp         = 40'h00_7000_0000;/" $BP_COMMON_DIR/src/include/bp_common_addr_pkgdef.svh
 sed -i "s/localparam bp_pc_entry_point_gp=39'h10_3000/localparam bp_pc_entry_point_gp=39'h00_7000_0000/" $BP_ME_DIR/test/common/bp_cce_mmio_cfg_loader.sv
 sed -i "s/wire local_cmd_li        = (proc_cmd_selected_lo.header.addr < dram_base_addr_gp);/wire local_cmd_li        = (proc_cmd_selected_lo.header.addr < 32'h5000_0000);/" $BP_TOP_DIR/src/v/bp_unicore_lite.sv
+
+## Replace $signed with signed' in all files due to Vivado bug for $signed
+find . -type f -exec sed -i 's/\$signed/signed'\''/g' {} +
+
+## Output new BlackParrot SHA
+echo "New BlackParrot SHA: $(git rev-parse --short HEAD)"
